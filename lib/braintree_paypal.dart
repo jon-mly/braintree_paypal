7359946e0bbc638625eb9698fa1ae6c196c761f8
jon-mly <jon.mly@gmail.com>
2023-11-14 12:24:43 +0100

    reverted file move

--- lib/braintree_paypal.dart
+++
@@ -1,6 +0,0 @@
-library braintree_paypal;
-
-export 'braintree_paypal_plugin.dart';
-export 'src/types/nonce.dart';
-export 'src/types/credit_card_request.dart';
-export 'src/types/paypal_request.dart';
---
+++ lib/src/braintree_paypal.dart
@@ -0,0 +1,6 @@
+library braintree_paypal;
+
+export '../braintree_paypal_plugin.dart';
+export 'types/nonce.dart';
+export 'types/credit_card_request.dart';
+export 'types/paypal_request.dart';
